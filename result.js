567941412210f673d91655b46632ac361c0a08a6
Arif Hosen <128309897+arifSDE@users.noreply.github.com>
2026-05-04 16:36:12 +0300

    Add files via upload

---
+++ result.js
@@ -0,0 +1,16 @@
+var result=50;
+if(result<0){
+    console.log("Failed");
+}
+else if(result>=0 && result<40){
+    console.log("Tumi C Grade paico");
+}
+else if(result>=40 && result<60){
+    console.log("Tumi B Grade Paico");
+}
+else if(result>=60 && result<80){
+    console.log("Tumi A- grade Paico")
+}
+else{
+    console.log("Invalid");
+}
