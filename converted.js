bba1adcfb72507e67212feac1c1aed28915593ec
Arif Hosen <128309897+arifSDE@users.noreply.github.com>
2026-05-04 16:24:25 +0300

    Add files via upload

---
+++ converted.js
@@ -0,0 +1,5 @@
+var num="12.5";
+var num1=15;
+var result=parseFloat(num)+num1;
+console.log(result);
+
---
+++ if_else.js
@@ -0,0 +1,10 @@
+var status="rain";
+if(status=="rain"){
+    console.log("Baire jawa jabe na");
+}
+else if (status==gorom){
+    console.log("Baire gorom beshi ")
+}
+else{
+    console.log("Baire jawa jabe")
+}
