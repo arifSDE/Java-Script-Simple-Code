9b8c27d8c3f6fc0ba09e57333c395b7b1d165047
Arif Hosen <128309897+arifSDE@users.noreply.github.com>
2026-05-07 19:21:54 +0300

    Add files via upload

---
+++ loop_2.js
@@ -0,0 +1,12 @@
+var friend=["Hero",5,"Alom",{name:"Jhon"},["Rohim","karim"]]
+
+for(var i=0; i<friend.length; i++){
+    var element=friend[i];
+
+    if(element=="Alom"){
+        console.log("Yes");
+    }
+    else{
+        console.log("NO");
+    }
+}
