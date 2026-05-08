6450c29633dd475ae0683abeb18769f9dd9de1a0
Arif Hosen <128309897+arifSDE@users.noreply.github.com>
2026-05-08 18:40:39 +0300

    looping

---
+++ loop_3.js
@@ -0,0 +1,18 @@
+var friend=["Hero",5,"Alom",{name:"Jhon"},["Rohim","karim"]]
+
+// for(var i=0; i<friend.length; i++)
+// {
+//     var element=friend[i];
+
+//     if(element=="Alom"){
+//         console.log("Yes Sir");
+//     }
+//     else{
+//         console.log("NO Sir");
+//     }
+// }
+
+for(var index=0; index<20; index++){
+    var element=index
+    console.log(element);
+}
